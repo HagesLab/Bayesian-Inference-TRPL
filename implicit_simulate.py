7873c88cf0963b4e4859f157d7e6d38cd69dbae9
cfai2304 <36453766+cfai2304@users.noreply.github.com>
2020-11-06 11:51:00 -0500

    Nondimensionalized eqns

--- implicit_simulate.py
+++ implicit_simulate.py
@@ -35,13 +35,12 @@ def pulse_laser_maxgen(max_gen, alpha, x_array):
     """
     return (max_gen * np.exp(-alpha * x_array))
 
-def time_step(current_N, current_P, current_E_field, prev_N, prev_P, prev_E_field, dx, dt, D_N, D_P, T, n0, p0, rr_rate, tau_N, tau_P, sf, sb, alpha_0, alpha_1, alpha_2):
+def time_step(current_N, current_P, current_E_field, prev_N, prev_P, prev_E_field, lamb, D_N, D_P, n0, p0, rr_rate, tau_N, tau_P, sf, sb, alpha_0, alpha_1, alpha_2):
     # Do 1st TS using 1st order backward
     new_N = current_N.copy()
     new_P = current_P.copy()
     new_E_field = current_E_field.copy()
-    
-    
+
     iter_ = 0
     while(True):
     
@@ -49,40 +48,40 @@ def time_step(current_N, current_P, current_E_field, prev_N, prev_P, prev_E_fiel
         mat_A_N = np.zeros((m, m))
         mat_A_P = np.zeros((m, m))
         
-        mat_A_N[0,0] = alpha_0 * (dx / dt) + D_N * (-q / (2*kB*T) * (new_E_field[1]) + (1/dx))
-        mat_A_P[0,0] = alpha_0 * (dx / dt) + D_P * (q / (2*kB*T) * (new_E_field[1]) + (1/dx))
-        mat_A_N[0,1] = D_N * (-q / (2*kB*T) * new_E_field[1] - (1/dx))
-        mat_A_P[0,1] = D_P * (q / (2*kB*T) * new_E_field[1] - (1/dx))
+        mat_A_N[0,0] = alpha_0 + D_N * (-1/2 * new_E_field[1] + 1)
+        mat_A_P[0,0] = alpha_0 + D_P * (1/2 * new_E_field[1] + 1)
+        mat_A_N[0,1] = D_N * (-1/2 * new_E_field[1] - 1)
+        mat_A_P[0,1] = D_P * (1/2 * new_E_field[1] - 1)
         
-        mat_A_N[m-1,m-1] = alpha_0 * (dx / dt) - D_N * (-q / (2*kB*T) * (new_E_field[m-1]) - (1/dx))
-        mat_A_P[m-1,m-1] = alpha_0 * (dx / dt) - D_P * (q / (2*kB*T) * (new_E_field[m-1]) - (1/dx))
-        mat_A_N[m-1,m-2] = -D_N * (-q / (2*kB*T) * new_E_field[m-1] + (1/dx))
-        mat_A_P[m-1,m-2] = -D_P * (q / (2*kB*T) * new_E_field[m-1] + (1/dx))
+        mat_A_N[m-1,m-1] = alpha_0 + D_N * (1/2 * new_E_field[m-1] + 1)
+        mat_A_P[m-1,m-1] = alpha_0 + D_P * (-1/2 * new_E_field[m-1] + 1)
+        mat_A_N[m-1,m-2] = D_N * (1/2 * new_E_field[m-1] - 1)
+        mat_A_P[m-1,m-2] = D_P * (-1/2* new_E_field[m-1] - 1)
         
         # for i in range(1, m-1):
-        #     mat_A_N[i,i-1] = -D_N * (-q / (2*kB*T) * new_E_field[i] + (1/dx))
-        #     mat_A_P[i,i-1] = -D_P * (q / (2*kB*T) * new_E_field[i] + (1/dx))
+        #     mat_A_N[i,i-1] = D_N * (1/2 * new_E_field[i] - 1)
+        #     mat_A_P[i,i-1] = D_P * (-1/2 * new_E_field[i] - 1)
             
-        #     mat_A_N[i,i] = (dx / dt) + D_N * (-q / (2*kB*T) * (new_E_field[i+1] - new_E_field[i]) + (2/dx))
-        #     mat_A_P[i,i] = (dx / dt) + D_P * (q / (2*kB*T) * (new_E_field[i+1] - new_E_field[i]) + (2/dx))
+        #     mat_A_N[i,i] = alpha_0 + D_N * (-1/2 * (new_E_field[i+1] - new_E_field[i]) + 2)
+        #     mat_A_P[i,i] = alpha_0 + D_P * (1/2 * (new_E_field[i+1] - new_E_field[i]) + 2)
             
-        #     mat_A_N[i,i+1] = D_N * (-q / (2*kB*T) * new_E_field[i+1] - (1/dx))
-        #     mat_A_P[i,i+1] = D_P * (q / (2*kB*T) * new_E_field[i+1] - (1/dx))
+        #     mat_A_N[i,i+1] = D_N * (-1/2 * new_E_field[i+1] - 1)
+        #     mat_A_P[i,i+1] = D_P * (1/2* new_E_field[i+1] - 1)
             
-        mat_A_N[1:-1,0:-2] += np.diag(-D_N * (-q / (2*kB*T) * new_E_field[1:-2] + (1/dx)), 0)
-        mat_A_P[1:-1,0:-2] += np.diag(-D_P * (+q / (2*kB*T) * new_E_field[1:-2] + (1/dx)), 0)
+        mat_A_N[1:-1,0:-2] += np.diag(D_N * (1/2* new_E_field[1:-2] - 1), 0)
+        mat_A_P[1:-1,0:-2] += np.diag(D_P * (-1/2 * new_E_field[1:-2] - 1), 0)
         
-        mat_A_N[1:-1,1:-1] += np.diag(alpha_0 * (dx / dt) + D_N * (-q / (2*kB*T) * (np.roll(new_E_field, -1)[1:-2] - new_E_field[1:-2]) + (2/dx)), 0)
-        mat_A_P[1:-1,1:-1] += np.diag(alpha_0 * (dx / dt) + D_P * (q / (2*kB*T) * (np.roll(new_E_field, -1)[1:-2] - new_E_field[1:-2]) + (2/dx)), 0)
+        mat_A_N[1:-1,1:-1] += np.diag(alpha_0 + D_N * (-1/2 * (np.roll(new_E_field, -1)[1:-2] - new_E_field[1:-2]) + 2), 0)
+        mat_A_P[1:-1,1:-1] += np.diag(alpha_0 + D_P * (1/2 * (np.roll(new_E_field, -1)[1:-2] - new_E_field[1:-2]) + 2), 0)
         
-        mat_A_N[1:-1,2:] += np.diag(D_N * (-q / (2*kB*T) * np.roll(new_E_field, -1)[1:-2] - (1/dx)))
-        mat_A_P[1:-1,2:] += np.diag(D_P * (q / (2*kB*T) * np.roll(new_E_field, -1)[1:-2] - (1/dx)))
+        mat_A_N[1:-1,2:] += np.diag(D_N * (-1/2 * np.roll(new_E_field, -1)[1:-2] - 1), 0)
+        mat_A_P[1:-1,2:] += np.diag(D_P * (1/2 * np.roll(new_E_field, -1)[1:-2] - 1), 0)
         
         rr = -rr_rate * (new_N * new_P - n0 * p0)
         nrr = -(new_N * new_P - n0 * p0) / ((tau_N * new_P) + (tau_P * new_N))
         
-        vec_B_N = (alpha_1 * current_N + alpha_2 * prev_N) * (dx/dt) + (rr + nrr) * dx
-        vec_B_P = (alpha_1 * current_P + alpha_2 * prev_P) * (dx/dt) + (rr + nrr) * dx
+        vec_B_N = (alpha_1 * current_N + alpha_2 * prev_N) + (rr + nrr)
+        vec_B_P = (alpha_1 * current_P + alpha_2 * prev_P) + (rr + nrr)
             
         # Boundary consumption terms lag one iteration behind
         f_0 = -sf * (new_N[0] * new_P[0] - n0 * p0) / (new_N[0] + new_P[0])
@@ -107,9 +106,9 @@ def time_step(current_N, current_P, current_E_field, prev_N, prev_P, prev_E_fiel
         #     A = 1 - (q_C * dt / eps) * (D_P * (-q / (2*kB*T)) * (new_P[i] + new_P[i-1]) + D_N * (-q / (2*kB*T)) * (new_N[i] + new_N[i-1]))
         #     new_E_field[i] = (A ** -1) * b
             
-        b2 = alpha_1 * current_E_field[1:-1] + alpha_2 * prev_E_field[1:-1] + (q_C * dt / eps) * (D_P/dx * (new_P[1:] - np.roll(new_P, 1)[1:]) - D_N/dx * (new_N[1:] - np.roll(new_N, 1)[1:]))
+        b2 = alpha_1 * current_E_field[1:-1] + alpha_2 * prev_E_field[1:-1] + lamb * (D_P * (new_P[1:] - np.roll(new_P, 1)[1:]) - D_N * (new_N[1:] - np.roll(new_N, 1)[1:]))
             
-        A2 = alpha_0 + (q_C * dt / eps) * (D_P * (q / (2*kB*T)) * (new_P[1:] + np.roll(new_P, 1)[1:]) + D_N * (q / (2*kB*T)) * (new_N[1:] + np.roll(new_N, 1)[1:]))
+        A2 = alpha_0 + (lamb/2) * (D_P * (new_P[1:] + np.roll(new_P, 1)[1:]) + D_N * (new_N[1:] + np.roll(new_N, 1)[1:]))
         new_E_field[1:-1] = (A2 ** -1) * b2
             
         iter_ += 1
@@ -131,9 +130,9 @@ if __name__ == "__main__":
     # Consumption rates
     sf = 5950 * (1e7) / (1e9)        # [cm / s] to [nm / ns]
     sb = 1e-6 * (1e7) / (1e9)       # [cm / s] to [nm / ns]
-    rr_rate = 0*1e-10 * ((1e7) ** 3) / (1e9)# [cm^3 / s] to [nm^3 / ns]
-    tau_N = 20000                      # [ns]
-    tau_P = 20000                      # [ns]
+    rr_rate = 1e-10 * ((1e7) ** 3) / (1e9)# [cm^3 / s] to [nm^3 / ns]
+    tau_N = 20                      # [ns]
+    tau_P = 20                      # [ns]
     
     # Other
     T = 300               # [K]
@@ -157,24 +156,25 @@ if __name__ == "__main__":
     P[:, 0] = pulse_laser_maxgen(1e17 * ((1e-7) ** 3), 1e5 * 1e-7, grid_node_x) + p0
     
     # Non dimensionalize
-    # n0 *= dx ** 3
-    # p0 *= dx ** 3
-    # N[:,0] *= dx ** 3
-    # P[:,0] *= dx ** 3
+    n0 *= dx ** 3
+    p0 *= dx ** 3
+    N[:,0] *= dx ** 3
+    P[:,0] *= dx ** 3
     
-    # D_N *= (dt * dx ** -2)
-    # D_P *= (dt * dx ** -2)
+    D_N *= (dt * dx ** -2)
+    D_P *= (dt * dx ** -2)
     
-    # rr_rate *= (dt * dx ** -3)
+    sf *= (dt * dx ** -1)
+    sb *= (dt * dx ** -1)
+    rr_rate *= (dt * dx ** -3)
     
-    # tau_N *= dt ** -1
-    # tau_P *= dt ** -1
-    # lamb = (q * q_C) / (eps * kB * T * dx)
+    tau_N *= dt ** -1
+    tau_P *= dt ** -1
+    lamb = (q * q_C) / (eps * kB * T * dx)
     # Do 1st TS using 1st order backward
         
-    iter_ = 0
     startTime = time.time()
-    params = (dx, dt, D_N, D_P, T, n0, p0, rr_rate, tau_N, tau_P, sf, sb)
+    params = (lamb, D_N, D_P, n0, p0, rr_rate, tau_N, tau_P, sf, sb)
     alphas = (1, 1, 0)
     alphas_2nd = (1.5, 2, -0.5)
     N[:,1], P[:,1], E_field[:,1] = time_step(N[:,0], P[:,0], E_field[:,0], N[:,-1], P[:,-1], E_field[:,-1], *params, *alphas)
@@ -184,6 +184,7 @@ if __name__ == "__main__":
         
     print("Took {} sec".format(time.time() - startTime))
     #plot the graph
+
     plt.figure(0)
     plt.yscale('log')
     plt.plot(grid_node_x, N[:,0], label="time: 0.0")
@@ -191,7 +192,7 @@ if __name__ == "__main__":
     plt.plot(grid_node_x, N[:,int(final_t*0.2/ dt)], label="time: 0.2")
     plt.plot(grid_node_x, N[:,int(final_t*0.5/ dt)], label="time: 0.5")
     plt.xlabel('x [nm]', fontsize = 15)
-    plt.ylabel('N [nm^-3]', fontsize = 15)
+    plt.ylabel('N* [unitless]', fontsize = 15)
     plt.title('electrons')
     plt.legend()
     
@@ -202,6 +203,6 @@ if __name__ == "__main__":
     plt.plot(grid_node_x, P[:,int(final_t*0.2/ dt)], label="time: 0.2")
     plt.plot(grid_node_x, P[:,int(final_t*0.5/ dt)], label="time: 0.5")
     plt.xlabel('x [nm]', fontsize = 15)
-    plt.ylabel('P [nm^-3]', fontsize = 15)
+    plt.ylabel('P* [unitless]', fontsize = 15)
     plt.title('holes')
     plt.legend()
