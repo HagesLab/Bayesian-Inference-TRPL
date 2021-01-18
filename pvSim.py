8bacae11053e02cdbde412c3519ab18b81819a64
cfai2304 <36453766+cfai2304@users.noreply.github.com>
2021-01-18 16:56:57 -0500

    More tweaks

--- pvSim.py
+++ pvSim.py
@@ -1,40 +1,28 @@
- # -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
-Created on Mon Nov  2 13:57:05 2020
+Created on Sat Dec 19 15:05:30 2020
 
-@author: cfai2
+@author: tladd
 """
 
-import math, numpy as np
+
+import numpy as np
 from numba import njit
 import time
 
 @njit(cache=True)
-def Sum(X):
-    Sum = 0
-    for x in X:
-        Sum += x
-    return Sum
-
-@njit(cache=True)
-def Norm(x, xO, tol):
-    Sum = SumX = 0
-    for n in range(len(x)):
-        Sum  += (x[n]-xO[n])*(x[n]-xO[n])
-        SumX += x[n]*x[n]
-    return math.sqrt(Sum/(SumX+tol))
-
-@njit(cache=True)
-def Copy(a, b):
-
-    n = len(a)
-    for k in range(n):
-        b[k] = a[k]
-
-@njit(cache=True)
-def Solve(A, b, c):
+def Solve(A, b, c):                                 # Solve with norm check
 
     n = len(b)
+    sumx = np.abs(A[1,0]*c[0]+A[0,1]*c[1] - b[0])   # norms
+    sumb = np.abs(b[0])
+    for k in range(1,n-1):
+        sumx += np.abs(A[2,k-1]*c[k-1]+A[1,k]*c[k]+A[0,k+1]*c[k+1] - b[k])
+        sumb += np.abs(b[k])
+    sumx += np.abs(A[2,-2]*c[-2]+A[1,-1]*c[-1] - b[-1])
+    sumb += np.abs(b[-1])
+
     for k in range(n-1):                            # Elimination
         q = A[2,k]/A[1,k]
         A[1,k+1] -= q*A[0,k+1]
@@ -43,119 +31,100 @@ def Solve(A, b, c):
     c[-1] = b[-1]/A[1,-1]                           # Backsubstitution
     for k in range(n-2,-1,-1):
         c[k] = (b[k]-A[0,k+1]*c[k+1])/A[1,k]
+    return sumx/sumb
 
 @njit(cache=True)
-def timeSteps(N, P, E, A, b, matPar, N0, P0, t, tol, MAX):
+def iterate(N, P, E, A, b, matPar, par):
 
 # Unpack local variables
-    TOL = 10.0**(-tol)
     N0, P0, DN, DP, rate, sr0, srL, tauN, tauP, Lambda = matPar
-    if t == 1:                                  # Select integration order
-        a0 = 1.0; a1 = -1.0; a2 = 0.0           # Euler step
-    else:
-        a0 = 1.5; a1 = -2.0; a2 = 0.5           # 2nd order implicit
-    k  = (t)%3
-    ko = (t-1)%3
-    kp = (t+1)%3
-    L  = len(N[0])                              # Get length
-    N[kp,:] = N[k,:]                            # Save current values
-    P[kp,:] = P[k,:]
-    E[kp,:] = E[k,:]
+    a0, a1, a2, k, kp, ko, L, tol , MAX = par
+    TOL  = 10.0**(-tol)
 
+# Iterate outer loop to convergence
     for iters in range(MAX):                    # Up to MAX iterations
         for n in range(1,L):                    # Solve for N
             A[0,n]   = DN*(-E[kp,n]/2 - 1)
             A[2,n-1] = DN*(+E[kp,n]/2 - 1)
         for n in range(L):
-            N[-1,n]  = N[kp,n]                  # Save previous iter
-            dRdn = -rate*P[kp,n] - ((P[kp,n])*(tauP*N[kp,n] + tauN*P[kp,n]) - tauP*(N[kp,n]*P[kp,n] - N0*P0))/(tauP*N[kp,n] + tauN*P[kp,n]) ** 2
-            A[1,n]   = a0 - A[0,n] - A[2,n] - dRdn
-            b[n]   = -(rate + 1/(tauP*N[kp,n] + tauN*P[kp,n])) * \
-                      (N[kp,n]*P[kp,n] - N0*P0) - dRdn * N[kp,n] - a1*N[k,n] - a2*N[ko,n]
-
-        ds0dn = -sr0*(P[kp,0]**2 + N0*P0) / (N[kp,0]+P[kp,0]) ** 2
-        dsLdn = -srL*(P[kp,-1]**2 + N0*P0) / (N[kp,-1]+P[kp,-1]) ** 2
-        A[1,0] -= ds0dn
-        A[1,-1] -= dsLdn
-        b[0]  -= sr0*(N[kp,0]*P[kp,0]  - N0*P0)/(N[kp,0]+P[kp,0]) + ds0dn * N[kp,0]
-        b[-1] -= srL*(N[kp,-1]*P[kp,-1] - N0*P0)/(N[kp,-1]+P[kp,-1]) + dsLdn * N[kp,-1]
-
-        Solve(A, b, N[kp])
+            np = N[kp,n]*P[kp,n] - N0*P0
+            tp = N[kp,n]*tauP + P[kp,n]*tauN
+            dR = -rate*P[kp,n] - (P[kp,n]*tp - tauP*np)/tp**2
+            A[1,n] = a0 - A[0,n] - A[2,n] - dR
+            b[n]   = -(rate + 1/tp)*np - dR*N[kp,n] - a1*N[k,n] - a2*N[ko,n]
+        ds0 = -sr0*(P[kp, 0]**2 + N0*P0)/(N[kp, 0]+P[kp, 0])**2
+        dsL = -srL*(P[kp,-1]**2 + N0*P0)/(N[kp,-1]+P[kp,-1])**2
+        A[1,0]  -= ds0
+        A[1,-1] -= dsL
+        b[0]  -= sr0*(N[kp, 0]*P[kp, 0]-N0*P0)/(N[kp, 0]+P[kp, 0]) + ds0*N[kp, 0]
+        b[-1] -= srL*(N[kp,-1]*P[kp,-1]-N0*P0)/(N[kp,-1]+P[kp,-1]) + dsL*N[kp,-1]
+        errN = Solve(A, b, N[kp])
 
         for n in range(1,L):                    # Solve for P
             A[0,n]   = DP*(+E[kp,n]/2 - 1)
             A[2,n-1] = DP*(-E[kp,n]/2 - 1)
         for n in range(L):
-            P[-1,n]  = P[kp,n]                  # Save previous iter
-            dRdp = -rate*N[-1,n] - ((N[-1,n])*(tauP*N[-1,n] + tauN*P[kp,n]) - tauN*(N[-1,n]*P[kp,n] - N0*P0))/(tauP*N[-1,n] + tauN*P[kp,n]) ** 2
-            A[1,n]   = a0 - A[0,n] - A[2,n] - dRdp
-            b[n]   = -(rate + 1/(tauP*N[-1,n] + tauN*P[kp,n])) * \
-                      (N[-1,n]*P[kp,n] - N0*P0) - dRdp * P[kp,n] - a1*P[k,n] - a2*P[ko,n]
-
-        ds0dp = -sr0*(N[-1,0]**2 + N0*P0) / (N[-1,0]+P[kp,0]) ** 2
-        dsLdp = -srL*(N[-1,-1]**2 + N0*P0) / (N[-1,-1]+P[kp,-1]) ** 2
-        A[1,0] -= ds0dp
-        A[1,-1] -= dsLdp
-
-        b[0]  -= sr0*(N[-1,0]*P[kp,0]  - N0*P0)/(N[-1,0]+P[kp,0]) + ds0dp * P[kp,0]
-        b[-1] -= srL*(N[-1,-1]*P[kp,-1] - N0*P0)/(N[-1,-1]+P[kp,-1]) + dsLdp * P[kp,-1]
-        Solve(A, b, P[kp])
+            np = N[kp,n]*P[kp,n] - N0*P0
+            tp = N[kp,n]*tauP + P[kp,n]*tauN
+            dR = -rate*N[kp,n] - (N[kp,n]*tp - tauN*np)/tp**2
+            A[1,n] = a0 - A[0,n] - A[2,n] - dR
+            b[n]   = -(rate + 1/tp)*np - dR*P[kp,n] - a1*P[k,n] - a2*P[ko,n]
+        ds0 = -sr0*(N[kp, 0]**2 + N0*P0)/(N[kp, 0]+P[kp, 0])**2
+        dsL = -srL*(N[kp,-1]**2 + N0*P0)/(N[kp,-1]+P[kp,-1])**2
+        A[1,0]  -= ds0
+        A[1,-1] -= dsL
+        b[0]  -= sr0*(N[kp, 0]*P[kp, 0]-N0*P0)/(N[kp, 0]+P[kp, 0]) + ds0*P[kp, 0]
+        b[-1] -= srL*(N[kp,-1]*P[kp,-1]-N0*P0)/(N[kp,-1]+P[kp,-1]) + dsL*P[kp,-1]
+        errP = Solve(A, b, P[kp])
 
         for n in range(1,L):                    # Solve for E
-            E[-1,n] = E[kp,n]
             A[0,n]  = Lambda*(DP*(P[kp,n]+P[kp,n-1]) + \
                               DN*(N[kp,n]+N[kp,n-1]))/2 + a0
-            b[n]    = Lambda*(DP*(P[kp,n]-P[kp,n-1]) - \
-                              DN*(N[kp,n]-N[kp,n-1])) - \
-                     a1*E[k,n] - a2*E[ko,n]
+            b[n]    = Lambda*(DP*(P[kp,n]-P[kp,n-1])  - \
+                              DN*(N[kp,n]-N[kp,n-1])) - a1*E[k,n] - a2*E[ko,n]
             E[kp,n] = b[n]/A[0,n]
 
-        normN = Norm(N[kp], N[-1], TOL/10)
-        normP = Norm(P[kp], P[-1], TOL/10)
-        #normE = Norm(E[kp], E[-1], TOL/10)
-#        print(iters, normN, normP, normE)
-        if ((normN < TOL and normP < TOL)):  break
+        if ((errN < TOL and errP < TOL)):  break
     return iters+1
 
 @njit(cache=True)
-def tEvol(N, P, E, plN, plP, plE, plI, A, b, matPar, \
-          Length, Time, L, T, plT, pT, tol, MAX):
+def tEvol(N, P, E, plN, plP, plE, plI, A, b, matPar, simPar):
+
+    Length, Time, L, T, plT, pT, tol, MAX = simPar
     N0   = matPar[:,0]
     P0   = matPar[:,1]
     rate = matPar[:,4]
-    fail_states = [0]*len(matPar)
-    # Threads = [449, 471, 481, 633, 758, 764, 768, 770, 776, 778, 782, 800, \
-    #             804, 808, 920, 926, 928, 932, 934, 940, 944, 946, 952, 955, \
-    #             956, 964, 968, 970, 1094, 1099, 1110, 1287, 1290, 1294, 2056, \
-    #           2062, 2064, 2066, 2068, 2080, 2084, 2102, 2214, 2226, 2236, \
-    #           2244, 2246, 2248, 2252, 2254, 2258, 2259, 2262, 2264, 2266, 2582]
-    #Threads = [920, 926, 2066, 2084, 2244]
-    #for thr in Threads:
-    #for thr in range(323,325):
-    for thr in range(len(matPar)):              # Loop over thrs
+    itrs = [0]*len(matPar)
 
-        print("thread: ", thr)
-        for t in range(1,T+1):                  # Outer time loop
-            iters = timeSteps(N[thr], P[thr], E[thr], A[thr], b[thr], matPar[thr], \
-                             N0[thr], P0[thr], t, tol, MAX)
+    for th in range(len(matPar)):               # Loop over threads
+        print("thread: ", th)
+        for t in range(T+1):                    # Outer time loop
+            if t == 0:                          # Select integration order
+                a0 = 1.0; a1 = -1.0; a2 = 0.0   # Euler step
+            else:
+                a0 = 1.5; a1 = -2.0; a2 = 0.5   # 2nd order implicit
+            kp  = (t+1)%3                       # new time
+            k   = (t)  %3                       # current time
+            ko  = (t-1)%3                       # old time
+            par = (a0, a1, a2, k, kp, ko, L, tol, MAX)
+            N[th,kp,:] = N[th,k,:]              # Save current values
+            P[th,kp,:] = P[th,k,:]
+            E[th,kp,:] = E[th,k,:]
+            iters = iterate(N[th], P[th], E[th], A[th], b[th], matPar[th], par)
+
+            if itrs[th] < iters:
+                itrs[th] = iters
             if iters >= MAX:
                 print("NO CONVERGENCE: ", iters, " iterations")
-                fail_states[thr] = -1
                 break
-            # else:
-            #     print("Converged after: ", iter, " iterations")
-
             if t%plT == 0:
-                plI[thr,t//plT] = Sum(rate[thr]*(N[thr,0]*P[thr,0] - \
-                                                 N0[thr]*P0[thr]))
+                plI[th,t//plT] = rate[th]*(N[th,k]*P[th,k] - N0[th]*P0[th]).sum()
             if t in pT:
                 ind = pT.index(t)
-                Copy(N[thr,t%3], plN[thr,ind])
-                Copy(P[thr,t%3], plP[thr,ind])
-                Copy(E[thr,t%3], plE[thr,ind])
-
-
-    return fail_states
+                plN[th, ind] = N[th,k]
+                plP[th, ind] = P[th,k]
+                plE[th, ind] = E[th,k]
+    return itrs
 
 def pvSim(matPar, simPar, iniPar):
 
@@ -172,9 +141,9 @@ def pvSim(matPar, simPar, iniPar):
 
     # Allocate arrays for each thread
     Threads = len(matPar)
-    N   = np.zeros((Threads,4,L))              # Include prior steps and iter
-    P   = np.zeros((Threads,4,L))
-    E   = np.zeros((Threads,4,L+1))
+    N   = np.zeros((Threads,3,L))              # Include prior steps and iter
+    P   = np.zeros((Threads,3,L))
+    E   = np.zeros((Threads,3,L+1))
     A   = np.zeros((Threads,3,L))              # Matrix arrays (inc E)
     b   = np.zeros((Threads,L))                # RHS vectors
     plI = np.zeros((Threads,T//plT+1))         # Arrays for plotting
@@ -183,26 +152,17 @@ def pvSim(matPar, simPar, iniPar):
     plE = np.zeros((Threads,len(pT),L+1))
 
     # Initialization - nodes at 1/2, 3/2 ... L-1/2
-    a, l = iniPar
+    a,l = iniPar
     a  *= dx3
     l  /= dx
-    N0   = matPar[:,0]
-    P0   = matPar[:,1]
-    rate = matPar[:,4]
-    x  = np.arange(L) + 0.5
-    dN = a * np.exp(-x/l)
+    x   = np.arange(L) + 0.5
+    dN  = a*np.exp(-x/l)
     N0, P0 = matPar[:,0:2].T
     N[:,0] = np.add.outer(N0, dN)
     P[:,0] = np.add.outer(P0, dN)
-    N[:,1] = N[:,0].copy()
-    P[:,1] = P[:,0].copy()
-    plN[:,0] = N[:,0]
-    plP[:,0] = P[:,0]
-    plE[:,0] = E[:,0]
-    plI[:,0] = rate[:]*((N[:,0]*P[:,0]).T - N0*P0).sum(axis=0)
 
     clock0 = time.time()
-    fail_states = tEvol(N, P, E, plN, plP, plE, plI, A, b, matPar, *simPar)
+    itrs = tEvol(N, P, E, plN, plP, plE, plI, A, b, matPar, simPar)
     print("Took {} sec".format(time.time() - clock0))
 
     plI *= dx**4/dt
@@ -210,22 +170,21 @@ def pvSim(matPar, simPar, iniPar):
     plP /= dx**3
     plE /= dx
 
-    return fail_states, (plN, plP, plE, plI)
+    return itrs, (plN, plP, plE, plI)
 
 if __name__ == "__main__":
 
     import pickle
     import csv
-    simPar, iniPar, matPar = pickle.load(open('hagesInputs300.pik', 'rb'))
-    #matPar = np.array(matPar[3457]).reshape((1, len(matPar[3457])))
-    pTh = -1
-    fail_states, pvOut = pvSim(matPar, simPar, iniPar)
+    simPar, iniPar, matPar = pickle.load(open('hagesInputs700.pik', 'rb'))
 
-    fail_states = np.array(fail_states).reshape((1, len(fail_states)))
-    # fail_states = np.concatenate((fail_states.T, matPar), axis=1)
-    # with open('fail3.csv', 'w+', newline='') as ofstream:
-    #     writer = csv.writer(ofstream, delimiter=',')
-    #     writer.writerows(fail_states)
+    itrs, pvOut = pvSim(matPar, simPar, iniPar)
 
-    pickle.dump(pvOut, open('hagesOut300.pik', 'wb'))
+    itrs = np.array(itrs).reshape((1, len(itrs)))
+    itrs = np.concatenate((itrs.T, matPar), axis=1)
+    with open('itrs.csv', 'w+', newline='') as ofstream:
+        writer = csv.writer(ofstream, delimiter=',')
+        writer.writerows(itrs)
+
+    pickle.dump(pvOut, open('hagesOut700sss.pik', 'wb'))
 
