acbf5545bd6a1aca4150ebb18ac872f30e6b1929
Calvin Fai <cfai2304@gmail.com>
2022-01-11 17:53:20 -0500

    Clean up filenames

--- parallel_bayes_gpu.py
+++ parallel_bayes_gpu.py
@@ -4,6 +4,7 @@ from numba import cuda
 from time import perf_counter
 import sys
 import numpy as np
+import os
 
 from bayeslib import bayes, get_initpoints, get_data, validate_ic_flags, validate_gpu_info, validate_IC
 from pvSimPCR import pvSim
@@ -20,10 +21,7 @@ unit_conversions = np.array([(1e7)**-3,(1e7)**-3,
 np.random.seed(42)
 if __name__ == "__main__":
 
-    ic_flags = {"time_cutoff":None,
-                "select_obs_sets":None,
-                "noise_level":1e15}
-    # simPar
+    # Set space and time grid options
     #Length = [311,2000,311,2000, 311, 2000]
     Length  = 2000                            # Length (nm)
     L   = 2 ** 7                                # Spatial points
@@ -34,47 +32,21 @@ if __name__ == "__main__":
 
     simPar = [Length, -1, L, -1, plT, pT, tol, MAX]
 
-    # iniPar and available modes
-    # 'exp' - parameters a and l for a*np.exp(-x/l)
-    # 'points' - direct list of dN [cm^-3] points as csv file read using get_initpoints()
-    #TODO: do away with init_mode
-    init_mode = "points"
-    #a  = 1e18/(1e7)**3                        # Amplitude
-    #l  = 100                                  # Length scale [nm]
-    #iniPar = np.array([[a, l]])
-    
-    try:
-        validate_ic_flags(ic_flags)
-    except Exception as e:
-        logging.error(e)
-        sys.exit(1)
-
-    iniPar = get_initpoints(r"/blue/c.hages/bay_inputs/{}".format(sys.argv[2]), ic_flags)
-    try:
-        validate_IC(iniPar, L)
-    except Exception as e:
-        logging.error(e)
-        sys.exit(1)
-
     # This code follows a strict order of parameters:
     # matPar = [N0, P0, DN, DP, rate, sr0, srL, tauN, tauP, Lambda, mag_offset]
-    # Parameter ranges
+    # Set the parameter ranges/sample space
     do_log = np.array([1,1,0,0,1,1,1,0,0,1,0])
     minX = np.array([1e8, 3e15, 4000, 4000, 4.8e-11, 1, 1, 1, 1, 10**-1, 0])
     maxX = np.array([1e8, 3e15, 4000, 4000, 4.8e-11, 1e5, 1e5, 1500, 3000, 10**-1, 0])
 
-    GPU_GROUP_SIZE = 2 ** 13                  # Number of simulations assigned to GPU at a time - GPU has limited mem$
-    num_gpus = 8
-    
+    # Other options
+    ic_flags = {"time_cutoff":None,
+                "select_obs_sets":None,
+                "noise_level":1e15}
+
     gpu_info = {"sims_per_gpu": 2 ** 13,
                 "num_gpus": 8}
 
-    try:
-        validate_gpu_info(gpu_info)
-    except Exception as e:
-        logging.error(e)
-        sys.exit(1)
-        
     sim_flags = {"load_PL_from_file": "load" in sys.argv[4],
                  "override_equal_mu":False,
                  "override_equal_s":False,
@@ -83,9 +55,35 @@ if __name__ == "__main__":
                  "random_sample":True,
                  "num_points":2**17}
 
-    experimental_data_filename = r"/blue/c.hages/bay_inputs/{}".format(sys.argv[1])
-    out_filename = sys.argv[3]
-    wdir = r"/blue/c.hages/bay_outputs/{}/".format(out_filename)
+    # Collect filenames
+    init_dir = r"/blue/c.hages/bay_inputs"
+    out_dir = r"/blue/c.hages/bay_outputs"
+    init_filename = os.path.join(init_dir, sys.argv[2])
+    experimental_data_filename = os.path.join(init_dir, sys.argv[1])
+    out_filename = os.path.join(out_dir, sys.argv[3])
+
+    # Get observations and initial condition
+    iniPar = get_initpoints(init_filename, ic_flags)
+    e_data = get_data(experimental_data_filename, ic_flags, sim_flags, scale_f=1e-23)
+
+    # Validate
+    try:
+        validate_ic_flags(ic_flags)
+    except Exception as e:
+        logging.error(e)
+        sys.exit(1)
+
+    try:
+        validate_IC(iniPar, L)
+    except Exception as e:
+        logging.error(e)
+        sys.exit(1)
+
+    try:
+        validate_gpu_info(gpu_info)
+    except Exception as e:
+        logging.error(e)
+        sys.exit(1)
 
     # Pre-checks
     try:
@@ -96,7 +94,6 @@ if __name__ == "__main__":
         assert (len(maxX) == num_params), "Missing max param values"
         assert all(minX <= maxX), "Min params larger than max params"
         
-
         print("Starting simulations with the following parameters:")
 
         print("Lengths: {}".format(Length))
@@ -107,13 +104,11 @@ if __name__ == "__main__":
             else:
                 print("{}: {} to {} {}".format(param_names[i], minX[i], maxX[i], "log" if do_log[i] else "linear"))
 
-        e_data = get_data(experimental_data_filename, ic_flags, sim_flags, scale_f=1e-23)
         assert len(iniPar) == len(e_data[0]), "Num. ICs mismatch num. datasets"
-        print("\nInitial condition - {}".format(iniPar))
+        print("\nInitial condition - {}".format(init_filename))
         print("\nExperimental data - {}".format(experimental_data_filename))
-
-        print(e_data)
         print("Output: {}".format(out_filename))
+
         try:
             print("Detecting GPU...")
             gpu_info["has_GPU"] = cuda.detect()
@@ -136,6 +131,10 @@ if __name__ == "__main__":
         print(oops)
         sys.exit(0)
 
+    print(ic_flags)
+    print(gpu_info)
+    print(sim_flags)
+
     minX *= unit_conversions
     maxX *= unit_conversions
 
@@ -149,25 +148,27 @@ if __name__ == "__main__":
     maxX /= unit_conversions
     X /= unit_conversions
 
+
     try:
-        import os
         print("Creating dir {}".format(out_filename))
-        os.mkdir(wdir)
+        os.mkdir(out_filename)
     except FileExistsError:
         print("{} dir already exists".format(out_filename))
     clock0 = perf_counter()
 
     try:
         print("Writing to /blue:")
-        np.save("{}_BAYRAN_P.npy".format(wdir + out_filename), P)
-        np.save("{}_BAYRAN_X.npy".format(wdir + out_filename), X)
+        base = os.path.basename(out_filename)
+        np.save(os.path.join(out_filename, "{}_BAYRAN_P.npy".format(base)), P)
+        np.save(os.path.join(out_filename, "{}_BAYRAN_X.npy".format(base)), X)
 
     except Exception as e:
         print(e)
         print("Write failed; rewriting to backup location /home:")
-        wdir = r"/home/cfai2304/super_bayes/"
-        np.save("{}_BAYRAN_P.npy".format(wdir + out_filename), P)
-        np.save("{}_BAYRAN_X.npy".format(wdir + out_filename), X)
+        sys.exit(0)
+        wdir = r"/home/cfai2304/super_bayes"
+        np.save(os.path.join(out_filename, "{}_BAYRAN_P.npy".format(base)), P)
+        np.save(os.path.join(out_filename, "{}_BAYRAN_X.npy".format(base)), X)
 
     print("Export took {}".format(perf_counter() - clock0))
 
