ce16349ada9dd1bf41088ecb692b2af624e99844
Calvin Fai <cfai2304@gmail.com>
2022-01-13 15:29:37 -0500

    Move (currently deprecated) grid search stuff into legacy

--- bayeslib.py
+++ bayeslib.py
@@ -11,34 +11,6 @@ import numpy as np
 
 from probs import prob, fastlog
 from bayes_io import save_raw_pl, load_raw_pl
-def indexGrid(N, refs):                        # Arrays of cell coordinates
-    cN  = N.copy()                             # Copy of cell indexes
-    K   = len(refs)                            # Num params
-    M   = len(refs[0])
-    pN  = np.ones(M, int)
-    indexes = np.zeros((len(N),M), int)        # Initialize indexes
-    for k in range(K):                         # Loop over refinement levels
-        ref = refs[K-k-1]                      # Loop backwards
-        ind = []                               # Indexes for current level
-        for m in range(len(ref)):              # Loop over directions
-            ind.append(cN%ref[m])              # Index for this direction
-            cN //= ref[m]                      # Divide out fom N
-        indexes += np.array(ind).T*pN          # Accumulate indexes from level
-        pN  *= ref                         # Update mutipliers
-    return indexes
-
-def paramGrid(ind, refs, minX, maxX):          # Arrays of parameters
-    global do_log
-    pN  = np.prod(refs, axis=0)                # Scale for indexes
-    X_lin   = minX + (maxX-minX)*(ind + 0.5)/pN    # Get params
-    X_log = minX * (maxX/minX)**((ind + 0.5)/pN)
-    return X_lin * (1-do_log) + X_log * do_log
-
-def refineGrid (N, ref):                       # Refine grid
-    siz = np.prod(ref)                         # Size of refined block
-    reN = np.arange(siz)                       # Refined indexes
-    N   = np.add.outer(reN, N*siz)             # 2D array of indexes
-    return N.flatten(order='F')                # Return flattened array
 
 def random_grid(minX, maxX, do_log, num_points, do_grid=False, refs=None):
     num_params = len(minX)
@@ -48,21 +20,12 @@ def random_grid(minX, maxX, do_log, num_points, do_grid=False, refs=None):
         if minX[i] == maxX[i]:
             grid[:,i] = minX[i]
         else:
-            #if do_grid:
-            #    ind = np.arange(refs[i])
-            #    if do_log[i]:
-            #        possible_vals = minX[i] * (maxX[i]/minX[i]) ** ((ind+0.5) / refs[i])
-            #    else:
-            #        possible_vals = minX[i] + (maxX[i]-minX[i]) * (ind+0.5) / refs[i]
-            #    grid[:,i] = np.random.choice(possible_vals, size=(len(grid[:,i],)))
-            #else:
             if do_log[i]:
                 grid[:,i] = 10 ** np.random.uniform(np.log10(minX[i]), np.log10(maxX[i]), (num_points,))
             else:
                 grid[:,i] = np.random.uniform(minX[i], maxX[i], (num_points,))
             
     return grid
-
 
 def make_grid(N, P, minX, maxX, do_log, sim_flags, nref=None, minP=None, refs=None):
     OVERRIDE_EQUAL_MU = sim_flags["override_equal_mu"]
@@ -76,6 +39,7 @@ def make_grid(N, P, minX, maxX, do_log, sim_flags, nref=None, minP=None, refs=No
         print(len(X), "random points")
 
     else:
+        raise NotImplementedError("Grid sample currently deprecated; set RANDOM_SAMPLE to true")
         #if P is not None:
         #    N   = N[np.where(np.sum(np.mean(P, axis=0), axis=1) > minP[nref])] # P < minP - avg over tfs, sum over mag
         N   = refineGrid(N, refs[nref])                      # Refine grid
@@ -174,12 +138,15 @@ def simulate(model, e_data, P, X, plI, num_curves,
                 if LOG_PL:
                     misc_time[gpu_id] += fastlog(plI[gpu_id], sys.float_info.min, TPB[0], num_SMs)
                 if "+" in sys.argv[4]:
-                    save_raw_pl(out_filename, ic_num, blk, plI[0])
+                    print("Warning: saving PL files WIP")
+                    
+                    #save_raw_pl(out_filename, ic_num, blk, plI[0])
                     
 
             else:
-                print("Loading plI group {}".format(blk))
-                plI = load_raw_pl(out_filename, ic_num, blk)
+                raise NotImplementedError("load PL not implemented")
+                #print("Loading plI group {}".format(blk))
+                #plI = load_raw_pl(out_filename, ic_num, blk)
 
             # Calculate errors
             err_sq_time[gpu_id] += prob(P[blk:blk+size], plI[gpu_id], values, std, np.ascontiguousarray(X[blk:blk+size, -1]), 
---
+++ legacy.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 13 15:23:05 2022
+
+@author: cfai2304
+"""
+import numpy as np
+
+def indexGrid(N, refs):                        # Arrays of cell coordinates
+    cN  = N.copy()                             # Copy of cell indexes
+    K   = len(refs)                            # Num params
+    M   = len(refs[0])
+    pN  = np.ones(M, int)
+    indexes = np.zeros((len(N),M), int)        # Initialize indexes
+    for k in range(K):                         # Loop over refinement levels
+        ref = refs[K-k-1]                      # Loop backwards
+        ind = []                               # Indexes for current level
+        for m in range(len(ref)):              # Loop over directions
+            ind.append(cN%ref[m])              # Index for this direction
+            cN //= ref[m]                      # Divide out fom N
+        indexes += np.array(ind).T*pN          # Accumulate indexes from level
+        pN  *= ref                         # Update mutipliers
+    return indexes
+
+def paramGrid(ind, refs, minX, maxX, do_log):          # Arrays of parameters
+    pN  = np.prod(refs, axis=0)                # Scale for indexes
+    X_lin   = minX + (maxX-minX)*(ind + 0.5)/pN    # Get params
+    X_log = minX * (maxX/minX)**((ind + 0.5)/pN)
+    return X_lin * (1-do_log) + X_log * do_log
+
+def refineGrid (N, ref):                       # Refine grid
+    siz = np.prod(ref)                         # Size of refined block
+    reN = np.arange(siz)                       # Refined indexes
+    N   = np.add.outer(reN, N*siz)             # 2D array of indexes
+    return N.flatten(order='F')                # Return flattened array
