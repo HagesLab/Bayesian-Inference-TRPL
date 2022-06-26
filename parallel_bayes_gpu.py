6e1aae82adad1324575e07bbc7005c4b87051949
Calvin Fai <cfai2304@gmail.com>
2022-06-25 21:36:47 -0400

    Configure for auger testing on GPU

--- parallel_bayes_gpu.py
+++ parallel_bayes_gpu.py
@@ -84,8 +84,12 @@ if __name__ == "__main__":
     # matPar = [N0, P0, DN, DP, rate, sr0, srL, CN, CP, tauN, tauP, Lambda, mag_offset]
     # Set the parameter ranges/sample space
     do_log = np.array([1,1,0,0,1,1,1,1, 1, 0,0,1,0])
-    minX = np.array([1e8, 1e14, 20, 20, 1e-11, 0.1, 0.1, 4.4e-29, 4.4e-29, 1, 1, 10**-1, 0])
-    maxX = np.array([1e8, 1e16, 20, 20, 1e-9, 1e2, 1e2, 4.4e-29, 4.4e-29, 1000, 2000, 10**-1, 0])
+    # minX = np.array([1e8, 1e14, 20, 20, 1e-11, 0.1, 0.1, 4.4e-29, 4.4e-29, 1, 1, 10**-1, 0])
+    # maxX = np.array([1e8, 1e16, 20, 20, 1e-9, 1e2, 1e2, 4.4e-29, 4.4e-29, 1000, 2000, 10**-1, 0])
+    
+    # Tests with CN only
+    minX = np.array([1e8, 3e15, 20, 20, 4.8e-11, 2, 2, 1e-30, 4.4e-29, 511, 871, 10**-1, 0])
+    maxX = np.array([1e8, 3e15, 20, 20, 4.8e-11, 2, 2, 1e-28, 4.4e-29, 511, 871, 10**-1, 0])
 
     # Other options
     # time_cutoff: Truncate observations with timestamps larger than time_cutoff.
@@ -97,7 +101,7 @@ if __name__ == "__main__":
 
     # sims_per_gpu: Number of simulations dispatched to GPU at a time. Adjust according to GPU mem limits.
     # num_gpus: Number of GPUs to attempt connecting to.
-    gpu_info = {"sims_per_gpu": 2 ** 2,    
+    gpu_info = {"sims_per_gpu": 2 ** 10,    
                 "num_gpus": 1}
 
 
@@ -114,7 +118,7 @@ if __name__ == "__main__":
                  "log_pl":True,
                  "self_normalize":False,
                  "random_sample":True,
-                 "num_points":2**3, 
+                 "num_points":2**10, 
                  }
 
     # Collect filenames
